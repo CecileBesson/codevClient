00c6549637237d73818c398800adbefd706c9375
CecileBesson <cecile.besson@live.fr>
2020-10-23 09:57:09 +0200

    +searchBarComponent

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,20 @@
 <template>
 <div>
   <h1>{{ $t("home.welcome") }}</h1>
+  <div>
+    <search-bar />
+  </div>
+
+
 </div>
 </template>
 
 <script>
+import searchBar from "@/components/SearchBar";
+
 export default {
   name: "Home",
+  components: {searchBar},
   methods: {
     getToken(){
       return localStorage.getItem("token")
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-card
+        class="pa-4"
+        flat
+        height="300px"
+
+    >
+      <v-toolbar
+          dense
+          floating
+      >
+        <v-text-field
+            hide-details
+            single-line
+            name="service"
+            label="Rechercher un service"
+        ></v-text-field>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+      </v-toolbar>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+name: "SearchBar.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
